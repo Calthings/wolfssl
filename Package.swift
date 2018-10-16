a376302fd46aedc69dd6344f7cb4587e8e96b8cc
Eric York <eric@calthings.com>
2018-10-16 09:55:45 -0700

    Test

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "wolfssl",
+    pkgConfig: "libwolfssl",
+//    providers: [
+//        .brew(["libwolfssl"])
+//    ]
+)
